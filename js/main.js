4b556192ea871da2814100175ad7af460db35b91
Chhavi Garg <chhavigarg1896@gmail.com>
2017-02-21 23:20:10 +0530

    my page added

---
+++ js/main.js
@@ -0,0 +1,34 @@
+function showIntro() {
+	document.getElementById('intro').className="final";
+	document.getElementById('intro').style.display="block";
+	document.getElementById('layer').style.display="block";
+}
+function hideIntro() {
+	document.getElementById('intro').className="init";
+	document.getElementById('intro').style.display="none";
+	document.getElementById('layer').style.display="none";
+}
+function showEducation()
+{
+	document.getElementById('edu').className="final";
+	document.getElementById('edu').style.display="block";
+	document.getElementById('layer').style.display="block";
+}
+function hideEducation()
+{
+	document.getElementById('edu').className="init";
+	document.getElementById('edu').style.display="none";
+	document.getElementById('layer').style.display="none";
+}
+function showProjects()
+{
+	document.getElementById('proj').className="final";
+	document.getElementById('proj').style.display="block";
+	document.getElementById('layer').style.display="block";
+}
+function hideProjects()
+{
+	document.getElementById('proj').className="init";
+	document.getElementById('proj').style.display="none";
+	document.getElementById('layer').style.display="none";
+}
